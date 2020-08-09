7ad1350fcac52f4abddec86f232351c452b7edc0
jobgit55 <jobmail55@163.com>
2020-08-09 17:43:47 +0900

    my ts axios commit

---
+++ demo_examples/progress/app.ts
@@ -0,0 +1,61 @@
+import axios from '../../src/index'
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
+
+const instance = axios.create()
+
+function calculatePercentage(loaded: number, total: number) {
+  return Math.floor(loaded * 1.0) / total
+}
+
+function loadProgressBar() {
+  const setupStartProgress = () => {
+    instance.interceptors.request.use(config => {
+      NProgress.start()
+      return config
+    })
+  }
+
+  const setupUpdateProgress = () => {
+    const update = (e: ProgressEvent) => {
+      console.log(e)
+      NProgress.set(calculatePercentage(e.loaded, e.total))
+    }
+    instance.defaults.onDownloadProgress = update
+    instance.defaults.onUploadProgress = update
+  }
+
+  const setupStopProgress = () => {
+    instance.interceptors.response.use(response => {
+      NProgress.done()
+      return response
+    }, error => {
+      NProgress.done()
+      return Promise.reject(error)
+    })
+  }
+
+  setupStartProgress()
+  setupUpdateProgress()
+  setupStopProgress()
+}
+
+loadProgressBar()
+
+const downloadEl = document.getElementById('download')
+
+downloadEl!.addEventListener('click', e => {
+  instance.get('https://picsum.photos/200/300')
+})
+
+const uploadEl = document.getElementById('upload')
+
+uploadEl!.addEventListener('click', e => {
+  const data = new FormData()
+  const fileEl = document.getElementById('file') as HTMLInputElement
+  if (fileEl.files) {
+    data.append('file', fileEl.files[0])
+
+    instance.post('/progress/upload', data)
+  }
+})
---
+++ src/defaults.ts
@@ -0,0 +1,43 @@
+import { AxiosRequestConfig } from './types'
+import { transformResponse, transformRequest } from './utils/data'
+import { processHeaders } from './utils/headers'
+
+const defaults: AxiosRequestConfig = {
+  method: 'get',
+  timeout: 0,
+
+  headers: {
+    common: {
+      Accept: 'application/json, text/plain, */*'
+    }
+  },
+
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+
+  transformRequest: [
+    function(data: any, headers: any): any {
+      processHeaders(headers, data)
+      return transformRequest(data)
+    }
+  ],
+  transformResponse: [
+    function(data: any): any {
+      return transformResponse(data)
+    }
+  ]
+}
+
+const methodsWithoutData = ['delete', 'get', 'head', 'options']
+methodsWithoutData.forEach(method => {
+  defaults.headers[method] = {}
+})
+
+const methodsWithData = ['post', 'put', 'patch']
+methodsWithData.forEach(method => {
+  defaults.headers[method] = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+
+export default defaults
